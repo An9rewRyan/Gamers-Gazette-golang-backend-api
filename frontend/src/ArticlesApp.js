95875676b4b837cb48a94a2e6eade5d3124a117a
Ерёменко Семён <deutschman1999@mail.ru>
2022-04-11 12:38:27 +0300

    ADD: react via separate dockerized app

---
+++ frontend/src/ArticlesApp.js
@@ -0,0 +1,51 @@
+import './App.css';
+import React from 'react';
+
+
+class ArticlesApp extends React.Component {
+   
+  constructor(props) {
+      super(props);
+ 
+      this.state = {
+          items: [],
+          DataisLoaded: false
+      };
+  }
+ 
+  componentDidMount() {
+      fetch(
+"http://localhost:8000/api/articles/")
+          .then((res) => res.json())
+          .then((json) => {
+              this.setState({
+                  items: json,
+                  DataisLoaded: true
+              });
+          })
+  }
+  render() {
+      const { DataisLoaded, items } = this.state;
+      if (!DataisLoaded) return <div>
+          <h1> Pleses wait some time.... </h1> </div> ;
+ 
+      return (
+      <div className = "App">
+          <h1> Fetch data from an api in react </h1>  {
+              items.map((item) => ( 
+              <ol key = { item.article_id } >
+                  Title: { item.title }, 
+                  Pub_date: { item.pub_date },
+                  Image_url: {item.image_url},
+                  Content: { item.content }, 
+                  Src_link: {item.src_link},
+                  Site_alias: {item.site_alias}
+                  </ol>
+              ))
+          }
+      </div>
+  );
+}
+}
+
+export default ArticlesApp;
---
+++ frontend/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import './index.css';
+import ArticlesApp from './ArticlesApp';
+import reportWebVitals from './reportWebVitals';
+
+const Routing = () => {
+  return(
+    <Router>
+      <Routes>
+        <Route exact path="articles/"  element={<ArticlesApp />} />
+        {/* <Route exact path="games/" element={<GamesApp />}/> */}
+      </Routes>
+    </Router>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Routing />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
