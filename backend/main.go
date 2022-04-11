62bbe295292de9e2d305805ad591a3df731d02bd
Ерёменко Семён <deutschman1999@mail.ru>
2022-04-12 00:15:32 +0300

    TEST: gorilla spa

--- backend/main.go
+++ backend/main.go
@@ -11,6 +11,45 @@ import (
 	"github.com/rs/cors"
 )
 
+type spaHandler struct {
+	staticPath string
+	indexPath  string
+}
+
+// ServeHTTP inspects the URL path to locate a file within the static dir
+// on the SPA handler. If a file is found, it will be served. If not, the
+// file located at the index path on the SPA handler will be served. This
+// is suitable behavior for serving an SPA (single page application).
+func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// get the absolute path to prevent directory traversal
+	path, err := filepath.Abs(r.URL.Path)
+	if err != nil {
+		// if we failed to get the absolute path respond with a 400 bad request
+		// and stop
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// prepend the path with the path to the static directory
+	path = filepath.Join(h.staticPath, path)
+
+	// check whether a file exists at the given path
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		// file does not exist, serve index.html
+		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
+		return
+	} else if err != nil {
+		// if we got an error (that wasn't that the file doesn't exist) stating the
+		// file, return a 500 internal server error and stop
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// otherwise, use http.FileServer to serve the static dir
+	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
+}
+
 func main() {
 	//uncomment on first launch and comment after sucess
 	// database.Create_basic_tables()
@@ -29,9 +68,12 @@ func main() {
 	auth := router.PathPrefix("/auth/").Subrouter()
 	routers.Route_api(api)
 	routers.Route_auth(auth)
-	router.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("../web"))))
+	spa := spaHandler{staticPath: "../web", indexPath: "index.html"}
+	router.PathPrefix("/").Handler(spa)
+
 	// router.PathPrefix("/articles/").Handler(http.FileServer(http.Dir("../web")))
-	router.PathPrefix("/articles/").Handler(http.StripPrefix("/articles/", http.FileServer(http.Dir("../web"))))
+	// router.PathPrefix("/articles/").Handler(http.StripPrefix("/articles/", http.FileServer(http.Dir("../web"))))
+	// router.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("../web"))))
 	fmt.Println("Server is listening....")
 
 	handler := cors.Handler(router)
