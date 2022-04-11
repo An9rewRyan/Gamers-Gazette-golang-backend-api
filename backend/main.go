1020af13e275849b6f2ff77843c9969fd149edf6
Ерёменко Семён <deutschman1999@mail.ru>
2022-04-12 00:00:55 +0300

    dont really care

--- backend/main.go
+++ backend/main.go
@@ -29,8 +29,9 @@ func main() {
 	auth := router.PathPrefix("/auth/").Subrouter()
 	routers.Route_api(api)
 	routers.Route_auth(auth)
-	router.PathPrefix("/").Handler(http.FileServer(http.Dir("../web")))
-	router.PathPrefix("/articles/").Handler(http.FileServer(http.Dir("../web")))
+	router.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("../web"))))
+	// router.PathPrefix("/articles/").Handler(http.FileServer(http.Dir("../web")))
+	router.PathPrefix("/articles/").Handler(http.StripPrefix("/articles/", http.FileServer(http.Dir("../web"))))
 	fmt.Println("Server is listening....")
 
 	handler := cors.Handler(router)
