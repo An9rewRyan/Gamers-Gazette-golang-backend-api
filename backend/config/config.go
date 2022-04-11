d4feee86a95f31095f16479d73dbf6dc82e78d3d
Ерёменко Семён <deutschman1999@mail.ru>
2022-04-11 16:19:14 +0300

    UPDATE: config.go

--- backend/config/config.go
+++ backend/config/config.go
@@ -1,4 +1,7 @@
 package config
 
+import "os"
+
 // const Db_conn_str string = "user=postgres password=1234 dbname=main_db sslmode=disable"
-const Db_conn_str string = "host=db user=postgres password=1234 dbname=main_db port=5432 sslmode=disable"
+// const Db_conn_str string = "host=db user=postgres password=1234 dbname=main_db port=5432 sslmode=disable"
+var Db_conn_str = os.Getenv("DATABASE_URL")
