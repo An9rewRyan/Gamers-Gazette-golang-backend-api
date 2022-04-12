099ca9b64f946a24af20cd464d10af44533caced
Ерёменко Семён <deutschman1999@mail.ru>
2022-04-12 17:43:53 +0300

    Update oauth2.go

--- backend/views/auth/oauth2.go
+++ backend/views/auth/oauth2.go
@@ -48,7 +48,7 @@ func Me(w http.ResponseWriter, r *http.Request) {
 	defer resp.Body.Close()
 	bytes, _ := ioutil.ReadAll(resp.Body)
 	fmt.Fprint(w, string(bytes))
-	url = fmt.Sprintf("https://api.vk.com/method/users.get?&access_token=%s&fields=%s&user_ids=%s&v=5.81", gjson.Get(string(bytes), "access_token"), scopeTemp, gjson.Get(string(bytes), "user_id"))
+	url = fmt.Sprintf("https://api.vk.com/method/users.get?&access_token=%s&fields=%s&user_ids=%s&v=5.131", gjson.Get(string(bytes), "access_token"), scopeTemp, gjson.Get(string(bytes), "user_id"))
 	fmt.Println(url)
 	req, err = http.NewRequest("GET", url, nil)
 	if err != nil {
