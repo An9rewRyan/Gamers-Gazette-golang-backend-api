f5f90275f7dde5e2cb837574f36f41f971436d1f
Ерёменко Семён <deutschman1999@mail.ru>
2022-04-12 19:32:28 +0300

    FIX: soc auth paths

--- backend/routers/auth_router_social.go
+++ backend/routers/auth_router_social.go
@@ -9,6 +9,9 @@ import (
 
 func Route_auth_social(mux *mux.Router) {
 	mux.HandleFunc("/vk", func(w http.ResponseWriter, r *http.Request) {
-		social_auth.Vk(w, r)
+		social_auth.Vk_redir(w, r)
+	})
+	mux.HandleFunc("/vk/me", func(w http.ResponseWriter, r *http.Request) {
+		social_auth.Vk_get_data(w, r)
 	})
 }
