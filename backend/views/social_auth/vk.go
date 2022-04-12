9ed637861265ba365a2c9d0ed05d7f91081c2c32
Ерёменко Семён <deutschman1999@mail.ru>
2022-04-12 20:12:11 +0300

    REMOVE: vkapi trash data

--- backend/views/social_auth/vk.go
+++ backend/views/social_auth/vk.go
@@ -28,17 +28,6 @@ func Vk_redir(w http.ResponseWriter, r *http.Request) {
 	newUrl := fmt.Sprintf("https://oauth.vk.com/authorize?response_type=code&client_id=%s&redirect_uri=%s&scope=%s", clientID, redirectURI, scopeTemp)
 	fmt.Println(newUrl)
 	http.Redirect(w, r, newUrl, http.StatusSeeOther)
-	// req, err := http.NewRequest("GET", url, nil)
-	// if err != nil {
-	// 	respErr(w, err)
-	// 	return
-	// }
-	// _, err = client.Do(req)
-	// if err != nil {
-	// 	respErr(w, err)
-	// 	return
-	// }
-	// defer resp.Body.Close()
 }
 
 func Vk_get_data(w http.ResponseWriter, r *http.Request) {
@@ -57,10 +46,8 @@ func Vk_get_data(w http.ResponseWriter, r *http.Request) {
 	}
 	defer resp.Body.Close()
 	bytes, _ := ioutil.ReadAll(resp.Body)
-	fmt.Fprint(w, string(bytes))
 	email := gjson.Get(string(bytes), "email")
 	url = fmt.Sprintf("https://api.vk.com/method/users.get?access_token=%s&fields=bdate&user_id=%s&v=5.131", gjson.Get(string(bytes), "access_token"), gjson.Get(string(bytes), "user_id"))
-	fmt.Println(url)
 	req, err = http.NewRequest("GET", url, nil)
 	if err != nil {
 		respErr(w, err)
@@ -77,11 +64,8 @@ func Vk_get_data(w http.ResponseWriter, r *http.Request) {
 		respErr(w, err)
 		return
 	}
-	fmt.Println(gjson.Get(string(bytes), "response.#.first_name"), gjson.Get(string(bytes), "response.#.last_name"), gjson.Get(string(bytes), "response.#.bdate"), email)
-	fmt.Fprint(w, gjson.Get(string(bytes), "response.#.first_name"), gjson.Get(string(bytes), "response.#.last_name"), gjson.Get(string(bytes), "response.#.bdate"), email)
-
 	user := structs.Soc_auth_data{
-		Username:  gjson.Get(string(bytes), "response.#.first_name").String() + " " + gjson.Get(string(bytes), "response.#.last_name").String(),
+		Username:  fmt.Sprintf("%s %s", gjson.Get(string(bytes), "response.#.first_name").String(), gjson.Get(string(bytes), "response.#.last_name").String()),
 		BirthDate: gjson.Get(string(bytes), "response.#.bdate").String(),
 		Email:     email.String(),
 	}
@@ -98,50 +82,3 @@ func respErr(w http.ResponseWriter, err error) {
 		log.Println(err)
 	}
 }
-
-// func Me(w http.ResponseWriter, r *http.Request) {
-// 	redirectURI := "https://gamersgazette.herokuapp.com/auth/me"
-// 	clientID := "8134856"
-// 	clientSecret := "7Vw4ALUIHMLPpHTKiRlG"
-// 	tr := &http.Transport{
-// 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-// 	}
-// 	client := &http.Client{Transport: tr}
-// 	code := r.URL.Query().Get("code")
-// 	if code == "" {
-// 		respErr(w, fmt.Errorf("code query param is not provided"))
-// 		return
-// 	}
-// 	fmt.Println(code, r.URL.RequestURI())
-// 	url := fmt.Sprintf("https://oauth.vk.com/access_token?grant_type=authorization_code&code=%s&redirect_uri=%s&client_id=%s&client_secret=%s", code, redirectURI, clientID, clientSecret)
-// 	req, _ := http.NewRequest("POST", url, nil)
-// 	resp, err := client.Do(req)
-// 	if err != nil {
-// 		respErr(w, err)
-// 		return
-// 	}
-// 	defer resp.Body.Close()
-// 	bytes, _ := ioutil.ReadAll(resp.Body)
-// 	fmt.Fprint(w, string(bytes))
-// 	email := gjson.Get(string(bytes), "email")
-// 	url = fmt.Sprintf("https://api.vk.com/method/users.get?access_token=%s&fields=bdate&user_id=%s&v=5.131", gjson.Get(string(bytes), "access_token"), gjson.Get(string(bytes), "user_id"))
-// 	fmt.Println(url)
-// 	req, err = http.NewRequest("GET", url, nil)
-// 	if err != nil {
-// 		respErr(w, err)
-// 		return
-// 	}
-// 	resp, err = client.Do(req)
-// 	if err != nil {
-// 		respErr(w, err)
-// 		return
-// 	}
-// 	defer resp.Body.Close()
-// 	bytes, err = ioutil.ReadAll(resp.Body)
-// 	if err != nil {
-// 		respErr(w, err)
-// 		return
-// 	}
-// 	fmt.Println(gjson.Get(string(bytes), "response.#.first_name"), gjson.Get(string(bytes), "response.#.last_name"), gjson.Get(string(bytes), "response.#.bdate"), email)
-// 	fmt.Fprint(w, gjson.Get(string(bytes), "response.#.first_name"), gjson.Get(string(bytes), "response.#.last_name"), gjson.Get(string(bytes), "response.#.bdate"), email)
-// }
