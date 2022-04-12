2078bc3cb4d571b59eee2302a69d5930024130a1
Ерёменко Семён <deutschman1999@mail.ru>
2022-04-12 14:54:55 +0300

    Update oauth2.go

--- backend/views/auth/oauth2.go
+++ backend/views/auth/oauth2.go
@@ -1,7 +1,11 @@
 package auth
 
 import (
+	"crypto/tls"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"strings"
 )
@@ -9,7 +13,7 @@ import (
 func Oauth2(w http.ResponseWriter, r *http.Request) {
 	clientID := "8134856"
 	redirectURI := "https://gamersgazette.herokuapp.com/auth/me"
-	scope := []string{"email", "account", "friends"}
+	scope := []string{"email", "account", "friends", "bdate"}
 	state := "12345"
 	scopeTemp := strings.Join(scope, "+")
 	url := fmt.Sprintf("https://oauth.vk.com/authorize?response_type=token&client_id=%s&redirect_uri=%s&scope=%s&state=%s", clientID, redirectURI, scopeTemp, state)
@@ -17,10 +21,10 @@ func Oauth2(w http.ResponseWriter, r *http.Request) {
 }
 
 func Me(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, r.URL.RequestURI())
-	// 	scope := []string{"email", "account", "friends", "bdate"}
-	// 	state := "12345"
-	// 	scopeTemp := strings.Join(scope, "+")
+	token := r.URL.Query().Get("access_token")
+	scope := []string{"email", "account", "friends", "bdate"}
+	// state := "12345"
+	scopeTemp := strings.Join(scope, "+")
 	// 	clientID := "8134856"
 	// 	redirectURI := "https://gamersgazette.herokuapp.com/auth/me"
 	// 	clientSecret := "7Vw4ALUIHMLPpHTKiRlG"
@@ -40,10 +44,10 @@ func Me(w http.ResponseWriter, r *http.Request) {
 	// 		respErr(w, fmt.Errorf("code query param is not provided"))
 	// 		return
 	// 	}
-	// 	tr := &http.Transport{
-	// 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	// 	}
-	// 	client := &http.Client{Transport: tr}
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
 	// 	url := fmt.Sprintf("https://oauth.vk.com/access_token?grant_type=authorization_code&code=%s&redirect_uri=%s&client_id=%s&client_secret=%s", code, redirectURI, clientID, clientSecret)
 	// 	req, _ := http.NewRequest("POST", url, nil)
 	// 	resp, err := client.Do(req)
@@ -58,29 +62,29 @@ func Me(w http.ResponseWriter, r *http.Request) {
 	// 	bytes, _ := ioutil.ReadAll(resp.Body)
 	// 	fmt.Fprint(w, string(bytes))
 	// 	json.Unmarshal(bytes, &token)
-	// 	url = fmt.Sprintf("https://api.vk.com/method/%s?v=5.81&access_token=%s&fields=%s", "users.get", token.AccessToken, scopeTemp)
-	// 	req, err = http.NewRequest("GET", url, nil)
-	// 	if err != nil {
-	// 		respErr(w, err)
-	// 		return
-	// 	}
-	// 	resp, err = client.Do(req)
-	// 	if err != nil {
-	// 		respErr(w, err)
-	// 		return
-	// 	}
-	// 	defer resp.Body.Close()
-	// 	bytes, err = ioutil.ReadAll(resp.Body)
-	// 	if err != nil {
-	// 		respErr(w, err)
-	// 		return
-	// 	}
-	// 	fmt.Fprint(w, string(bytes))
-	// }
+	url := fmt.Sprintf("https://api.vk.com/method/%s?v=5.81&access_token=%s&fields=%s", "users.get", token, scopeTemp)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		respErr(w, err)
+		return
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		respErr(w, err)
+		return
+	}
+	defer resp.Body.Close()
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		respErr(w, err)
+		return
+	}
+	fmt.Fprint(w, string(bytes))
+}
 
-	// func respErr(w http.ResponseWriter, err error) {
-	// 	_, er := io.WriteString(w, err.Error())
-	// 	if er != nil {
-	// 		log.Println(err)
-	// 	}
+func respErr(w http.ResponseWriter, err error) {
+	_, er := io.WriteString(w, err.Error())
+	if er != nil {
+		log.Println(err)
+	}
 }
