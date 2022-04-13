18c368fed7e7a3ac954c06335385d1e9abffe9c7
Ерёменко Семён <deutschman1999@mail.ru>
2022-04-13 14:17:52 +0300

    DELETE: commented useless functionality

--- backend/main.go
+++ backend/main.go
@@ -10,51 +10,11 @@ import (
 	"github.com/rs/cors"
 )
 
-// type spaHandler struct {
-// 	staticPath string
-// 	indexPath  string
-// }
-
-// // ServeHTTP inspects the URL path to locate a file within the static dir
-// // on the SPA handler. If a file is found, it will be served. If not, the
-// // file located at the index path on the SPA handler will be served. This
-// // is suitable behavior for serving an SPA (single page application).
-// func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-// 	// get the absolute path to prevent directory traversal
-// 	path, err := filepath.Abs(r.URL.Path)
-// 	if err != nil {
-// 		// if we failed to get the absolute path respond with a 400 bad request
-// 		// and stop
-// 		http.Error(w, err.Error(), http.StatusBadRequest)
-// 		return
-// 	}
-
-// 	// prepend the path with the path to the static directory
-// 	path = filepath.Join(h.staticPath, path)
-
-// 	// check whether a file exists at the given path
-// 	_, err = os.Stat(path)
-// 	if os.IsNotExist(err) {
-// 		// file does not exist, serve index.html
-// 		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
-// 		return
-// 	} else if err != nil {
-// 		// if we got an error (that wasn't that the file doesn't exist) stating the
-// 		// file, return a 500 internal server error and stop
-// 		http.Error(w, err.Error(), http.StatusInternalServerError)
-// 		return
-// 	}
-
-// 	// otherwise, use http.FileServer to serve the static dir
-// 	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
-// }
-
 func main() {
 	//uncomment on first launch and comment after sucess
 	// database.Create_basic_tables()
 	cors := cors.New(cors.Options{
-		AllowedOrigins: []string{"http://localhost:3000"},
-		// AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowedOrigins:   []string{"http://localhost:3000"},
 		AllowCredentials: true,
 	})
 	router := mux.NewRouter()
@@ -64,8 +24,6 @@ func main() {
 	routers.Route_api(api)
 	routers.Route_auth_basic(basic_auth)
 	routers.Route_auth_social(soc_auth)
-	// spa := spaHandler{staticPath: "../web", indexPath: "index.html"}
-	// router.PathPrefix("/").Handler(spa)
 
 	fmt.Println("Server is listening....")
 
